6afa2ce0714f98371c917466bcb096e5819e8200
ishakos <moh.ferdjani@gmail.com>
2024-08-03 13:51:57 +0100

    Updates

---
+++ precache-manifest.03d42a4a82f3ff0881c52d024ae8dad3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d9025fe2e85c955c7a9640509dba6d",
+    "url": "/Prextures/index.html"
+  },
+  {
+    "revision": "64cffd658522dc54a87c",
+    "url": "/Prextures/static/css/main.4269d9d8.chunk.css"
+  },
+  {
+    "revision": "56330017cf4e5c6572a6",
+    "url": "/Prextures/static/js/2.126bbd00.chunk.js"
+  },
+  {
+    "revision": "c912927bccf782ea9793fc47400e4250",
+    "url": "/Prextures/static/js/2.126bbd00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722d3bd116073611a1b4",
+    "url": "/Prextures/static/js/3.50f2e815.chunk.js"
+  },
+  {
+    "revision": "64cffd658522dc54a87c",
+    "url": "/Prextures/static/js/main.bc8151b0.chunk.js"
+  },
+  {
+    "revision": "551719872a321d517b48",
+    "url": "/Prextures/static/js/runtime-main.5f4d3a4b.js"
+  },
+  {
+    "revision": "20fe3f99c14d17a3aff5c1145c87a927",
+    "url": "/Prextures/static/media/emirates.20fe3f99.jpg"
+  },
+  {
+    "revision": "66f9ec03df510198e276f5d6a00921f4",
+    "url": "/Prextures/static/media/home.66f9ec03.jpg"
+  }
+]);
